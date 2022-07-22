77154d2c20950e3d6dc4baba036ec9f5ca3c54c7
Ivayim <arnaud.debona@live.be>
2022-07-22 10:49:24 +0200

    Add working script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // All credits accorded to https://github.com/wesbos & big thanks !
+
+/* document.addEventListener('keypress', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('65').audio.play();
+    }
+  }); */
+
+  document.addEventListener('keydown' , event => {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if(!audio) return;
+    audio.play();
+    })
